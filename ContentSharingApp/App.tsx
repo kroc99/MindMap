9dc617646555655fb0f5061e0a6d4e4df7f14481
Kristian O’Connor <kristianoconnor@ufl.edu>
2024-08-01 15:44:37 -0400

    Removed submodules and integrated ContentSharingApp and FrameDisplayApp

---
+++ ContentSharingApp/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>
+        {/* Change this text based on the app */}
+        Welcome to the Content Sharing App!
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
